f7abcd19fdd7ff9bed32501e4730fd5d3b94858b
enzo <enzo.vp@outlook.com>
2026-05-01 15:53:46 -0500

    initial project

---
+++ src/main/java/com/soa/soausuarios/SoausuariosApplication.java
@@ -0,0 +1,13 @@
+package com.soa.soausuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoausuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoausuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soa/soausuarios/SoausuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soa.soausuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoausuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
